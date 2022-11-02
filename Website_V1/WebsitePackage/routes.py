ff54a51d8f70ea1f176a26ab3954e54988e874b1
GiammaFer75 <giammafer@yahoo.com>
2022-11-02 17:58:53 +0000

    minor changes

--- Website_V1/WebsitePackage/routes.py
+++ Website_V1/WebsitePackage/routes.py
@@ -15,16 +15,18 @@ session = Session()
 @Cov19_msc.route("/Home")
 def home_page():
   logo_filename = os.path.join(Cov19_msc.config['UPLOAD_FOLDER'], 'cropped-Covid19-MSC.png')
-  subject_group_table = session.query(SubjectGroup)
+  # subject_group_table = session.query(SubjectGroup)
 
-  print('Current number of records - ',session.query(SubjectGroup).count())
 
-  df = controller.return_SubjectGroup_table()
+  # print('Current number of records - ',session.query(SubjectGroup).count())
 
+  # df = controller.return_SubjectGroup_table()
+
+  # return render_template("Home.html", 
+  #                        logo_image = logo_filename, 
+  #                        subject_group_table=df) #subject_group_table.all()
   return render_template("Home.html", 
-                         logo_image = logo_filename, 
-                         subject_group_table=df) #subject_group_table.all()
-
+                          logo_image = logo_filename)
 
 
 @Cov19_msc.route("/page1")
@@ -75,37 +77,44 @@ def Delete_Record():
 
 @Cov19_msc.route("/Create_Dummy_Database")
 def Create_DD():
+  '''
+  Create the dummy database from the scratch
+  '''
   logo_filename = os.path.join(Cov19_msc.config['UPLOAD_FOLDER'], 'cropped-Covid19-MSC.png')
-  #metadata_obj.create_all(engine)
-  list_id = session.query(SubjectGroup.group_id)
-  for g_id in list_id.all():
-    print(g_id[0])
-    session.query(SubjectGroup).filter(SubjectGroup.group_id == g_id[0]).delete(synchronize_session=False)
-    session.commit()
+  # #metadata_obj.create_all(engine)
+  # list_id = session.query(SubjectGroup.group_id)
+  # for g_id in list_id.all():
+  #   print(g_id[0])
+  #   session.query(SubjectGroup).filter(SubjectGroup.group_id == g_id[0]).delete(synchronize_session=False)
+  #   session.commit()
 
-  # Dummy table
-  subject_group_table = [{'group_id': '001', 'description': 'Hospitalized subjects', 'rep':['p','m','z']},
-        {'group_id': '002', 'description': 'Young students'       , 'rep':['p','z']},
-        {'group_id': '003', 'description': 'Female from Wales'    , 'rep':['p','m','z']},
-        {'group_id': '004', 'description': 'Male from London'     , 'rep':['p','m']},
-        {'group_id': '005', 'description': 'Scottish healthcare personnel', 'rep':['m']},
-        {'group_id': '006', 'description': 'Commonwealth research', 'rep':['p']}]
+  # # Dummy table
+  # subject_group_table = [{'group_id': '001', 'description': 'Hospitalized subjects', 'rep':['p','m','z']},
+  #       {'group_id': '002', 'description': 'Young students'       , 'rep':['p','z']},
+  #       {'group_id': '003', 'description': 'Female from Wales'    , 'rep':['p','m','z']},
+  #       {'group_id': '004', 'description': 'Male from London'     , 'rep':['p','m']},
+  #       {'group_id': '005', 'description': 'Scottish healthcare personnel', 'rep':['m']},
+  #       {'group_id': '006', 'description': 'Commonwealth research', 'rep':['p']}]
   
 
-  descript_vect = ['Male London', 'Female Surrey', 'Health Workers', 'Elder subjects', 
-                   'Liverpool group', 'Subjects over 80', 'Male Surrey', 'Health Workers 40'
-  ]
-  repos_vect = ['PRIDE,MetaboLight', 'ZENODO', 'PRIDE,ZENODO','PRIDE,MetaboLight,ZENODO',
-                'PRIDE, ZENODO', 'MetaboLight,ZENODO', 'PRIDE,MetaboLight,ZENODO', 'PRIDE,MetaboLight,ZENODO'
-  ]
+  # descript_vect = ['Male London', 'Female Surrey', 'Health Workers', 'Elder subjects', 
+  #                  'Liverpool group', 'Subjects over 80', 'Male Surrey', 'Health Workers 40'
+  # ]
+  # repos_vect = ['PRIDE,MetaboLight', 'ZENODO', 'PRIDE,ZENODO','PRIDE,MetaboLight,ZENODO',
+  #               'PRIDE, ZENODO', 'MetaboLight,ZENODO', 'PRIDE,MetaboLight,ZENODO', 'PRIDE,MetaboLight,ZENODO'
+  # ]
 
-  subj_group_table = SubjectGroup 
-  for ind, desc in enumerate(descript_vect):
-      new_group = subj_group_table(description=desc, repositories=repos_vect[ind]) # It is a class of type SubjectGroup with the attributes
-      session.add(new_group)
-      session.commit()
+  # subj_group_table = SubjectGroup 
+  # for ind, desc in enumerate(descript_vect):
+  #     new_group = subj_group_table(description=desc, repositories=repos_vect[ind]) # It is a class of type SubjectGroup with the attributes
+  #     session.add(new_group)
+  #     session.commit()
 
-  print(session.query(SubjectGroup).count())
+  # print(session.query(SubjectGroup).count())
+  from WebsitePackage import generate_dummy_db 
+
+  generate_dummy_db.main()
+
   return render_template("Create_DD.html",
                          logo_image = logo_filename)
 
