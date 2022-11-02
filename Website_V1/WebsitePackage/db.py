47d661a59a3c21597a2d26bbb1379a3b3cd766d8
GiammaFer75 <giammafer@yahoo.com>
2022-11-02 17:53:52 +0000

    minor changes

--- Website_V1/WebsitePackage/db.py
+++ Website_V1/WebsitePackage/db.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 Base = declarative_base()
-engine = create_engine('sqlite:///Cov19_database.db', echo=True, 
+engine = create_engine('sqlite:///Cov19_database.db', echo=False, 
 						connect_args={'check_same_thread':False})
 metadata_obj = MetaData()
 Session = sessionmaker(bind=engine)
