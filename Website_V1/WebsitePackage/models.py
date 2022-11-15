7e49e98c37fa208216f5a15bf5b8b18ff39f636e
GiammaFer75 <giammafer@yahoo.com>
2022-11-15 18:38:55 +0000

    Added the new database structure that solves the linkage between subject-samples-sites

--- Website_V1/WebsitePackage/models.py
+++ Website_V1/WebsitePackage/models.py
@@ -1,7 +1,13 @@
 from sqlalchemy import Column, String, Integer, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from WebsitePackage.db import Base, engine, Session, metadata_obj
-#
+
+
+
+
+# ********************************************************** #
+# ****************** FIRST VERSION ************************* #
+# ********************************************************** #
 
 
 class SubjectGroup(Base):
@@ -103,6 +109,107 @@ class Researchers(Base):
 	# RELATIONSHIP --- Researchers
 	#projects       = relationship("Projects", back_populates="researchers")
 	projects = relationship("Projects", secondary=Project_Research, back_populates="researchers")
+
+
+# ********************************************************** #
+# ****************** SECOND VERSION ************************ #
+# ********************************************************** #
+
+
+sample_sampleTypes  = Table("sample_sampletypes",
+							Base.metadata,
+							Column("sample_id",      ForeignKey("samples.sample_id"), primary_key=True),
+							Column("sample_type_id", ForeignKey("sample_types.sample_type_id"), primary_key=True)
+							)
+
+sample_analysisType = Table("sample_analysistypes",
+							Base.metadata,
+							Column("sample_id",        ForeignKey("samples.sample_id"), primary_key=True),
+							Column("analysis_type_id", ForeignKey("analysis_types.analysis_type_id"), primary_key=True)
+							)
+
+
+class Subjects_Groups(Base):
+	__tablename__ = "subjects_groups"
+
+	subjects_group_id 	= Column(String, primary_key=True)
+	site_id  			= Column(Integer, ForeignKey("sites.site_id"))
+
+	# RELATIONSHIP --- Subjects_Group
+	sites 				= relationship("Sites",    back_populates="subjects_groups")
+	subjects         	= relationship("Subjects", back_populates="subjects_groups")
+
+class Subjects(Base):
+	__tablename__ = "subjects"
+
+	subject_id   		= Column(String, primary_key=True)
+	subject_group_id	= Column(String, ForeignKey("subjects_groups.subjects_group_id"))
+
+	# RELATIONSHIP --- Subjects
+	subjects_groups		= relationship("Subjects_Groups", back_populates="subjects")
+	samples   			= relationship("Samples",         back_populates="subjects")
+
+class Samples(Base):
+	__tablename__ = "samples"
+
+	sample_id 			= Column(Integer, autoincrement=True, primary_key=True)
+	subject_id 			= Column(String,  ForeignKey("subjects.subject_id"))
+	event_id 			= Column(Integer, ForeignKey("events.event_id"))
+	site_id 			= Column(Integer, ForeignKey("sites.site_id"))
+
+	sample_types        = relationship("Sample_Types", secondary=sample_sampleTypes, 
+									   back_populates="samples")
+	analysis_types      = relationship("Analysis_Types", secondary=sample_analysisType,
+		                               back_populates="samples")
+
+	# RELATIONSHIP --- Samples
+	subjects 			= relationship("Subjects", back_populates="samples")
+	events				= relationship("Events",   back_populates="samples")
+	sites    			= relationship("Sites",    back_populates="samples")
+
+
+class Sites(Base):
+	__tablename__ = "sites"
+
+	site_id 			= Column(Integer, autoincrement=True, primary_key=True)
+	site_name 			= Column(String)
+
+	# RELATIONSHIP --- Sites
+	subjects_groups		= relationship("Subjects_Groups", back_populates="sites")
+	samples   			= relationship("Samples",         back_populates="sites")
+
+class Events(Base):
+	__tablename__ = "events"
+
+	event_id			= Column(Integer, autoincrement=True, primary_key=True)
+	event_name			= Column(String)
+
+	# RELATIONSHIP --- Events
+	samples   			= relationship("Samples",         back_populates="events")
+	#subjects_groups   	= relationship("Subjects_Groups", back_populates="events")
+
+
+class Sample_Types(Base):
+	__tablename__ = "sample_types"
+
+	sample_type_id 		= Column(Integer, autoincrement=True, primary_key=True)
+	sample_type_name 	= Column(String)
+
+	# RELATIONSHIP --- Sample_Types
+	samples 			= relationship("Samples", secondary=sample_sampleTypes,
+										back_populates="sample_types")
+
+class Analysis_Types(Base):
+	__tablename__ = "analysis_types"
+	
+	analysis_type_id	= Column(Integer, autoincrement=True, primary_key=True)
+	analysis_type_name	= Column(String)
+
+	samples        		= relationship("Samples", secondary=sample_analysisType,
+										back_populates="analysis_types")
+
+
+
 # always initialise with create_all
 Base.metadata.create_all(engine)
 
