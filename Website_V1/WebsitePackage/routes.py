fe5304a93c59fbb28a0895e05cb9911f7b92be61
GiammaFer75 <giammafer@yahoo.com>
2022-11-23 11:41:44 +0000

    Moved the " if request.method == "POST": " condition from /Search to /Home. Now the data inputed in the /Search are printed in the console when /Home page is reloaded after the clicking the Submit button in /Search. This is possible because the "request.form.get()" methods called in /Home return the data input in /Search.

--- Website_V1/WebsitePackage/routes.py
+++ Website_V1/WebsitePackage/routes.py
@@ -2,7 +2,7 @@
 from WebsitePackage import Cov19_msc 
 from WebsitePackage import db
 from WebsitePackage.db import Base, Session, engine, metadata_obj
-from WebsitePackage.models import Subjects_Groups
+from WebsitePackage.models import Subjects_Groups, Sample_Types, Analysis_Types
 from flask import Flask, render_template, url_for, request
 import os, sqlalchemy
 import json
@@ -10,21 +10,43 @@ from sqlalchemy import delete
 
 from WebsitePackage import controller
 
-session = Session()
 
+
+sample_types_table = Sample_Types
 
 @Cov19_msc.route("/")
-@Cov19_msc.route("/Home")
+@Cov19_msc.route("/Home", methods=['GET', 'POST'])
 def home_page():
   logo_filename = os.path.join(Cov19_msc.config['UPLOAD_FOLDER'], 'cropped-Covid19-MSC.png')
-  print('Current number of records - ',session.query(Subjects_Groups).count())
   
+  if request.method == "POST":
+    print('\nIM IN POST CONDITION\n')
+    form_subjects_group = request.form.get("subjects_group")
+    form_subject        = request.form.get("subject")
+    form_event          = request.form.get("event")
+    form_site           = request.form.get("site")
+    form_sample_type    = request.form.get("sample_type")
+    form_analysis       = request.form.get("analysis_type")
+
+    print(f"""
+          Subjects Group : {form_subjects_group}
+          Subject        : {form_subject}
+          Event          : {form_event}
+          Site           : {form_site}
+          Sample Type    : {form_sample_type}
+          Analysis       : {form_analysis}
+
+           """)
+
+  session = Session()
+  print('Current number of records - ',session.query(Subjects_Groups).count())
+
+  session.close()
+
   df = controller.return_SubjectGroup_table()
   return render_template("Home.html", 
                          logo_image = logo_filename, 
                          subject_group_table=df) #subject_group_table.all()
-  # return render_template("Home.html", 
-  #                         logo_image = logo_filename)
 
 
 @Cov19_msc.route("/page1")
@@ -45,31 +67,25 @@ def page1():
 def Search():
   logo_filename = os.path.join(Cov19_msc.config['UPLOAD_FOLDER'], 'cropped-Covid19-MSC.png')
 
+  session = Session()
+  sample_types_table = session.query(Sample_Types)                # Fetch SAMPLE TYPES items
+  sample_types_items = [item.sample_type_name for item in sample_types_table.all()]
+  analysis_types_table = session.query(Analysis_Types)
+  analysis_types_items = [item.analysis_type_name for item in analysis_types_table.all()]
+  session.close()
+  
   if request.method == "POST":
-
-    if request.data != '':
+    try:
       form_data = json.loads(request.data)
-      print(form_data["sample_type"])
-    print(request.data)
-
-    form_subjects_group = request.form.get("subjects_group")
-    form_subject        = request.form.get("subject")
-    form_event          = request.form.get("event")
-    form_site           = request.form.get("site")
-    form_sample_type    = request.form.get("sample_type")
-    form_analysis       = request.form.get("analysis")
-
-    print(f"""
-          Subjects Group : {form_subjects_group}
-          Subject        : {form_subject}
-          Event          : {form_event}
-          Site           : {form_site}
-          Sample Type    : {form_sample_type}
-          Analysis       : {form_analysis}
-
-           """)
+      for key, item in form_data.items():
+         print(key, item)
+    except:
+      pass
+  
   return render_template("Search.html",
-                         logo_image = logo_filename)
+                         logo_image = logo_filename,
+                         sample_types_items = sample_types_items,
+                         analysis_types_items=analysis_types_items)
 
 @Cov19_msc.route("/New_Record", methods=['GET', 'POST'])
 def New_Record():
