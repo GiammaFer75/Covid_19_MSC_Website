056058b8d2f1316f23eb7b08527bab7147e92c68
GiammaFer75 <giammafer@yahoo.com>
2022-12-02 15:47:35 +0000

    Added the command to connect to the bit.io database host. It works but is super slow and the Base.metadata.drop_all(engine) and Base.metadata.create_all(engine) do not work. However, this is the first connection to an external database.

--- Website_V1/WebsitePackage/db.py
+++ Website_V1/WebsitePackage/db.py
@@ -5,8 +5,17 @@ from sqlalchemy.orm import sessionmaker
 
 
 Base = declarative_base()
+
+# ********* LOCAL DB ********* #
 engine = create_engine('sqlite:///Cov19_database.db', echo=False, 
 						connect_args={'check_same_thread':False})
+
+# ********* EXTERNAL DB ********* #
+# engine = create_engine('postgresql://Covid19MSC:v2_3wK5L_S9wisniwuVBYbsP2jWusbBT@db.bit.io/Covid19MSC/Covid19MSC',
+# 	                   pool_pre_ping=True)
+# REFERENCES
+# https://stackoverflow.com/questions/65657767/psycopg2-operationalerror-ssl-syscall-error-eof-detected-on-flask-sqlaclemy-ce
+
 metadata_obj = MetaData()
 Session = sessionmaker(bind=engine)
 
